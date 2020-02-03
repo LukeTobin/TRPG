d49a3f7d37fee0f86e0039f05fbffdb6dee54887
LukeTobin <lukejltobin@gmail.com>
2020-02-03 02:18:36 +0100

    Unity
    - Added Unity files

---
+++ RL-TRPG/Assets/Scripts/Grid.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Grid
+{
+    int width;
+    int height;
+    float cellSize;
+    int[,] gridArray;
+
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white);
+            }
+        }
+    }
+
+    Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+}
